31e9cab4adc2cfccd2ca8f4f57cd71b1c524eb31
Lorna Jane Mitchell <lorna@lornajane.net>
2017-07-26 14:21:11 +0100

    Better message wording for new questions

--- scripts/incoming-data.js
+++ scripts/incoming-data.js
@@ -2,21 +2,30 @@ module.exports = function(robot) {
   return robot.router.post('/hubot/stackoverflow/incoming', function(req, res) {
     var room;
     room = "advo-stack-overflow-t";
+    console.log(req.body);
+    var q;
+    if(req.body.data.question) {
+        q = req.body.data.question;
+    }
     // robot.messageRoom(room, "A hook! value of " + req.body.a);
 
     var message = {
         attachments:
         [
             {
-                title: "Question about Cloudant",
+                title: q.title,
                 color: "#e09900",
-                text: "this is my message, it contains more words and maybe the title?",
+                text: "<" + q.link + "| view on stackoverflow> or <https://sodashboard.mybluemix.net/home.html#edit?" + q.question_id + "| view on the dashboard>",
                 fields:
                     [
                     {
                         title: "Author",
-                        value: "Jane Doe",
+                        value: "<" + q.owner.link + "|" + q.owner.display_name + ">",
                         short: true
+                    },
+                    {   title:"Tagged",
+                        value: q.tags.join(", "),
+                        short:true
                     }
                     ]
             
